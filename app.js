93632320878aa20487d59341f0469721f5b4c92b
changjamie <ririchiyo8242@gmail.com>
2021-07-04 23:55:28 +0800

    Restaurant Project

---
+++ app.js
@@ -0,0 +1,43 @@
+// app.js
+// require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+
+// require handlebars in the project
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurants: restaurant })
+})
+
+// 依餐廳名稱與種類與類別搜尋
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLocaleLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
+
